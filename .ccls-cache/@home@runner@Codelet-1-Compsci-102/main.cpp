bbe53bd4c6e89070d20c7d1b19cfc85cf559d057
Sean Kondracki <>
2022-08-29 18:17:10 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Codelet-1-Compsci-102/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>2
+
+using namespace std;
+
+int main(){
+  int first = 0; 
+  int second = 0;
+cout << "Enter one integer: ";
+cin >> first;
+cout << "Enter another integer: ";
+cin >> second;
+  int sum = first + second;
+  cout << "Their sum is " << sum;
+
+  
+}
